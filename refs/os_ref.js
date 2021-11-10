675bd05257aa710e347962ac08d381f1ab623a69
cricket <rakhaevpavel@gmail.com>
2021-11-10 18:17:42 +0300

    Operation System (os) module

---
+++ refs/os_ref.js
@@ -0,0 +1,24 @@
+const os = require('os');
+
+// Operation System
+
+// Платформа
+// console.log(os.platform());
+
+// Архитектура
+// console.log(os.arch());
+
+// Общая информация
+// console.log(os.cpus());
+
+// Свободная память
+// console.log(os.freemem());
+
+// Сколько всего памяти
+// console.log(os.totalmem());
+
+// Корневая директория
+// console.log(os.homedir());
+
+// Сколько система работает
+console.log(os.uptime());
