7d8422a9cd97754b69b8ece9cd0ef72784116988
cricket <rakhaevpavel@gmail.com>
2021-12-04 13:39:54 +0300

    App setup & work w/ HTML files

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    if (req.url === '/') {
-      fs.readFile(
-        path.join(__dirname, 'views', 'index.html'),
-        'utf-8',
-        (err, data) => {
-          if (err) throw err;
-
-          res.end(data);
-        }
-      );
-    } else if (req.url === '/about') {
-      fs.readFile(
-        path.join(__dirname, 'views', 'about.html'),
-        'utf-8',
-        (err, data) => {
-          if (err) throw err;
-
-          res.end(data);
-        }
-      );
-    } else if (req.url === '/api/users') {
-      res.writeHead(200, {
-        'Content-Type': 'text/json'
-      });
-
-      const users = [
-        { name: 'Vladilen', age: 25 },
-        { name: 'Elena', age: 23 },
-      ];
-
-      res.end(JSON.stringify(users));
-    }
-  } else if (req.method === 'POST') {
-    const body = [];
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    req.on('data', data => {
-      body.push(Buffer.from(data));
-    });
-
-    req.on('end', () => {
-      const message = body.toString().split('=')[1];
-
-      res.end(`
-        <h1>Ваше сообщение: ${message.toString()}</h1>
-      `);
-    });
-  }
+app.get('/', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'views', 'index.html')
+  );
 });
 
-server.listen(3000, () => {
-  console.log('Server is running...');
+app.get('/about', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'views', 'about.html')
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
