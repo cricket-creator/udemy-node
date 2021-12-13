15c36712242eece2cce6807a18a89edfdd77909f
Pavel Rakhaev <rakhaevpavel@gmail.com>
2021-12-13 18:44:32 +0300

    Transition to MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 // Добавляем роуты
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
+const ordersRoutes = require('./routes/orders');
+
+const User = require('./models/user');
 
 const app = express();
 // Создаём движок
 const hbs = exphbs.create({
   defaultLayout: 'main',
+  handlebars: allowInsecurePrototypeAccess(handlebars),
   extname: 'hbs'
 });
 // Отслеживаем созданный движок
@@ -19,6 +26,19 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 // Отслеживаем шаблоны
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+  try {
+
+    const user = await User.findById('61b7496b28cfd279cb8e5cbf');
+    req.user = user;
+    next();
+  } catch (err) {
+
+    console.log(err);
+  }
+});
+
 // Определяем папку public как статичную
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -27,9 +47,38 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start () {
+
+  try {
+
+    const url = 'mongodb+srv://cricket:KQcUPjh3i7uXPNA@cluster0.ohtep.mongodb.net/shop';
+    await mongoose.connect(url, { useNewUrlParser: true });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: 'pavel@mail.ru',
+        name: 'Pavel',
+        cart: { items: [] },
+      });
+      await user.save();
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+
+    console.log(err);
+  }
+}
+
+start();
+
+/**
+ * user: cricket
+ * password: KQcUPjh3i7uXPNA
+ * url: mongodb+srv://cricket:KQcUPjh3i7uXPNA@cluster0.ohtep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+ */
---
+++ routes/orders.js
@@ -0,0 +1,50 @@
+const { Router } = require('express');
+const Order = require('../models/order');
+const router = Router();
+
+router.get('/', async (req, res) => {
+  try {
+
+    const orders = await Order.find({ 'user.userId': req.user._id }).populate('user.userId');
+
+    res.render('orders', {
+      isOrders: true,
+      title: 'Заказы',
+      orders: orders.map(order => ({
+        ...order._doc,
+        price: order.courses.reduce((total, course) => total += course.count * course.course.price, 0)
+      }))
+    });
+  } catch (err) {
+
+    console.log(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+
+    const user = await req.user.populate('cart.items.courseId');
+    const courses = user.cart.items.map(item => ({
+      count: item.count,
+      course: { ...item.courseId._doc }
+    }));
+    const order = new Order({
+      user: {
+        name: req.user.name,
+        userId: req.user,
+      },
+      courses,
+    });
+
+    await order.save();
+    await req.user.clearCart();
+
+    res.redirect('/orders');
+  } catch (err) {
+
+    console.log(err);
+  }
+});
+
+module.exports = router;
